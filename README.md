d404f3c854d833696612dec368db37d779455c27
tceyhan <55tc155@gmail.com>
2022-03-12 03:30:32 +0300

    changed.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # JS---Project-09_Weather
+1-https://openweathermap.org/ sitesinden üye olundu.
+2-key al
+3-api koduna yapıştır fetch yaparken
+4-lang kısmı optional olarak verilmiş değiştirebilirsin.
+5-çıkan şehir/kasaba tıklayınca google araması ve vikipedia araması çıkar.
+6-card üzerine gelindiğinde hover gradient renk uygulandı
+7-buton hover uygulandı bootstrap class verilerek
+8-h1 elementi yakalanarak js sayfasında animasyon opacity verildi.
+
+
--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const inputCity = document.getElementById("input");
 const submitCity = document.getElementById("submit");
-const cardContainer = document.getElementById("hava-kart");
+const cardContainer = document.getElementById("air_card");
+
+let h1 = document.querySelector('h1');
+let cardBody = document.getElementById("cardbody");
+
+
+inputCity.style.backgroundColor = "khaki";
+
+h1.style.fontSize = "50px";
+h1.animate([
+  {opacity: 0},
+  {opacity: 1}
+], {
+  duration: 1000,
+  iterations: 3
+});
+
 
 async function get(url) {
   let response = await fetch(url);
@@ -8,19 +24,18 @@ async function get(url) {
   return data;
 }
 
+
 submitCity.addEventListener("click", addToCard);
 
 function addToCard(e) {
   console.log(inputCity.value);
-  get(
-    `http://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=1f200bfdffc562d6b20713c8e197849d&lang=tr&units=metric`
-  )
+  get(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=1f200bfdffc562d6b20713c8e197849d&lang=tr&units=metric`)
     .then((result) => {
       console.log(result);
       cardContainer.innerHTML += `
         <div class="col">
          <div class="card">
-          <div class="card-body">
+          <div class="card-body" id ="cardbody">
             <h5 class="card-title">${result.name}<sup>${result.sys.country}</sup></h5>
             <p class="card-text">
               <img src="http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png" alt="">
@@ -28,15 +43,26 @@ function addToCard(e) {
             <p class="card-text">
               ${Math.round(result.main.temp)}
             </p>
-            <p class="card-text">
+            <p class="card-text durum" >
             ${result.weather[0].description}
             </p>
           </div>
         </div>
        </div>
-        `;
+        `;    
+                   
     })
+
     .catch((err) => {});
 
+ 
   e.preventDefault();
 }
+
+cardContainer.addEventListener("click", (google) => {
+      window.open(`https://www.google.com/search?q=${inputCity.value}`);
+      window.open(`https://tr.wikipedia.org/w/index.php?search=${inputCity.value}&title=%C3%96zel:Ara&profile=advanced&fulltext=1&ns0=1`);
+      window.open(`https://github.com/tceyhan?tab=repositories`);
+      e.preventDefault();
+
+  });
